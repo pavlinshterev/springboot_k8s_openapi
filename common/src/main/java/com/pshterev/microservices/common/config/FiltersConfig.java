de5bfb8adde3c283a19619ceef4427d6d982fb4c
Pavlin Shterev <pshterev@twocachalotes.com>
2022-08-26 20:10:16 +0300

    - add doc explaining why the x-forwarded-port header is overwritten on the requests

--- common/src/main/java/com/pshterev/microservices/common/config/FiltersConfig.java
+++ common/src/main/java/com/pshterev/microservices/common/config/FiltersConfig.java
@@ -22,6 +22,16 @@ public class FiltersConfig {
    @Bean
    public FilterRegistrationBean<ConfigureRequestHeadersFilter> configureRequestHeadersFilter(
            @Value("${server.port:1}") String serverPort) {
+
+      // nginx ingress controller sets the x-forwarded-port header value to the port on which
+      // the nginx listens which is not the same as the node port (that is the port on which the
+      // APIs are publicly available). By manually overriding the value for 'x-forwarded-port'
+      // header we help Spring Data Rest to produce correct REST links. The value that we use is
+      // the 'server.port' property from Spring configuration (which is the port on which the
+      // Tomcat server is listening inside the container) and for convenience this is the same
+      // port on which the nginx load balancer listens to and which is used to access the services
+      // from outside the K8s cluster machine
+
       LOGGER.info(">>>> Creating ConfigureRequestHeadersFilter <<<<");
       Map<String, List<String>> newHeaderValuesMap = new HashMap<>();
       newHeaderValuesMap.put("x-forwarded-port", List.of(serverPort));
